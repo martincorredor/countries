73255b3761cb8ed757e4280faa3fb050b56867c1
Martin Corredor <dev.martincorredor@gmail.com>
2025-07-08 17:30:06 -0500

    ADD. Add Header styles.

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Where in the world?</h1>
+    <header className={styles.header}>
+      <h1 className={styles.title}>Where in the world?</h1>
       <ThemeToggle />
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
